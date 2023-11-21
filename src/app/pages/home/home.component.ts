457320d438481ad89f2ff210c47d9249460d409e
DannielNavas <dannielnavas@gmail.com>
2023-11-21 06:45:09 -0500

    ✨ Add Task interface and update home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,50 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  tasks = signal(['task 1', 'task 2', 'task 3']);
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Task 1',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: 'Task 2',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: 'Task 3',
+      completed: false,
+    },
+  ]);
 
   changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newValue = input.value;
-    this.tasks.update((tasks) => [...tasks, newValue]);
+    // this.tasks.update((tasks) => [...tasks, newValue]);
+    this.addTask(newValue);
+  }
+
+  addTask(title: string) {
+    const newTask: Task = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+    this.tasks.update((tasks) => [
+      ...tasks,
+      newTask,
+    ]);
   }
 
   deleteTask(index: number) {
