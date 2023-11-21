8be379a05e3715f6c784323ca5c5bd9919f729fe
DannielNavas <dannielnavas@gmail.com>
2023-11-21 06:29:48 -0500

    ✨ Fix task rendering in home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +9,5 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  tasks = signal(['task 1', 'task 2', 'task 3'])
 }
