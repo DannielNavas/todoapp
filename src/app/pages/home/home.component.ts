fbfbb112de2b28dbd7be47f9e8eca152247965b8
DannielNavas <dannielnavas@gmail.com>
2023-11-21 06:37:50 -0500

    ✨ Add changeHandler and deleteTask methods

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,5 +9,15 @@ import { Component, signal } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks = signal(['task 1', 'task 2', 'task 3'])
+  tasks = signal(['task 1', 'task 2', 'task 3']);
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.tasks.update((tasks) => [...tasks, newValue]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.filter((_, i) => i !== index));
+  }
 }
