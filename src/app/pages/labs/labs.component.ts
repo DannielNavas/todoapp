1fd227c9d3b23ad57ec20cc4493d67808a9eee9a
DannielNavas <dannielnavas@gmail.com>
2023-11-21 06:01:23 -0500

    ✨ Add property binding and update component data

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -6,10 +6,18 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrl: './labs.component.css',
 })
 export class LabsComponent {
   title = 'Labs';
   name = 'Danniel Navas';
   age = 33;
+  urlImage = 'https://picsum.photos/200/300';
+  disabled = true;
+  persona = {
+    name: 'Danniel',
+    age: 33,
+    country: 'Colombia',
+    avatar: 'https://picsum.photos/200/300',
+  };
 }
